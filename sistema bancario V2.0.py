b736f3924b7b7a53506ced50fefe76dfd4e8e98d
Avanildo Rodrigues <avanildorodrigues17@gmail.com>
2023-08-01 21:08:58 -0300

    System V2.0

--- sistema bancario V2.0.py
+++ sistema bancario V2.0.py
@@ -1,17 +1,19 @@
 import textwrap
 from coresptbr import cores
 
+
 def menu():
-    menu = '''\n
+    opc = '''\n
     ============ MENU ============
     [d]\tDepositar
     [s]\tSacar
     [e]\tExtrato
     [nc]\tNova conta
+    [nu]\tNovo usuário
     [lc]\tListar contas
     [q]\tSair
     =>'''
-    return input(textwrap.dedent(menu))
+    return input(textwrap.dedent(opc))
 
 
 def depositar(saldo, valor, extrato, /):
@@ -49,6 +51,56 @@ def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
 
     return saldo, extrato
 
+
+def exibir_extrato(saldo, /, *, extrato):
+    print('\n============ EXTRATO ============')
+    print('Não foram realizadas movimentações.' if not extrato else extrato)
+    print(f'\nSaldo:\t\tR$ {saldo:.2f}')
+
+
+def criar_usuario(usuarios):
+    cpf = input('Informe o CPF (somente números): ')
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print(cores.colorir('\nJá exite usuario com esse CPF!', 'vm'))
+        return
+
+    nome = input('Informe o nome completo: ')
+    data_nascimento = input('Informe a data de nascimento (dd-mm-aaaa): ')
+    endereco = input('Informe o endereço (longradouro, num - bairro - cidade/sigla estado): ')
+
+    usuarios.append({'nome': nome, 'data_nascimento': data_nascimento, 'cpf': cpf, 'endereco': endereco})
+
+    print(cores.colorir('Usuário criado com sucesso!', 'vd'))
+
+
+def filtrar_usuario(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario['cpf'] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
+
+def criar_conta(agencia, numero_conta, usuarios):
+    cpf = input('Informe o CPF do usuário: ')
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print(cores.colorir('Conta criada com sucesso!', 'vd'))
+        return {'agencia': agencia, 'numero_conta': numero_conta, 'usuario': usuario}
+    print(cores.colorir('\nUsuário não encontrado, fluxo de criação de conta encerrado!', 'vm'))
+
+
+def listar_contas(contas):
+    for conta in contas:
+        linha = f'''\
+            Agencia:\t{conta["agencia"]}
+            C/C:\t\t{conta["numero_conta"]}
+            Titular:\t{conta["usuario"]["nome"]}
+            '''
+        print('=' * 100)
+        print(textwrap.dedent(linha))
+
+
 def main():
     LIMITE_SAQUES = 3
     AGENCIA = '0001'
@@ -75,7 +127,7 @@ def main():
                 valor=valor,
                 extrato=extrato,
                 limite=limite,
-                numero_saques,
+                numero_saques=numero_saques,
                 limite_saques=LIMITE_SAQUES,
             )
         elif opcao == 'e':
@@ -90,3 +142,15 @@ def main():
 
             if conta:
                 contas.append(conta)
+
+        elif opcao == 'lc':
+            listar_contas(contas)
+
+        elif opcao == 'q':
+            break
+
+        else:
+            print(cores.colorir('Operação inválida! Por favor selecione novamente a operação desejada.', 'vm'))
+
+
+main()
